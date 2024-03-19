506c9681284f96d0cec46a97b0a435d5e3d6b73a
최유정 <117022954+YUJEONGCHOII@users.noreply.github.com>
2024-03-20 00:45:54 +0900

    [level 0] Title: 대문자와 소문자, Time: 10.64 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for(int i=0; i<my_string.length(); i++){
+            if(!Character.isUpperCase(my_string.charAt(i))){
+                answer += (my_string.charAt(i)+"").toUpperCase();
+            } else {
+                answer += (my_string.charAt(i)+"").toLowerCase();
+            }
+        }
+        
+        return answer;
+    }
+}
