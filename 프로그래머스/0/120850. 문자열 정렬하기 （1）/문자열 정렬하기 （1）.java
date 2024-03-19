a2ade3f89b5d43f837811c96da37caa62e23f572
최유정 <117022954+YUJEONGCHOII@users.noreply.github.com>
2024-03-20 02:39:28 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.51 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String my_string) {
+        String[] str = my_string.replaceAll("[^0-9]", "").split("");
+        
+        int[] answer = new int[str.length];
+        
+        for(int i=0; i<str.length; i++){
+            answer[i] = Integer.parseInt(str[i]);
+        }
+        
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
