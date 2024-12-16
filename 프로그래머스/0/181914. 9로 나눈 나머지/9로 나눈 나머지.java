2da24a40e9ad26712ff1b0a15a55e283d28c8632
최유정 <117022954+YUJEONGCHOII@users.noreply.github.com>
2024-12-16 23:51:16 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 53.77 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String number) {
+        String[] str = number.split("");
+        int answer = 0;
+        
+        for(int i=0; i<str.length; i++){
+            answer += Integer.parseInt(str[i]);
+        }
+        
+        return answer%9;
+    }
+}
